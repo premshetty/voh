0dc68d7175c09592a6eada5baf2ff15e93c161aa
PREM KUMAR <premenaga@gmail.com>
2023-01-05 16:22:33 +0530

    tailwind config color variables added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'head_text': '#333333',
+      'button_cta': '#A84266',
+      'para_text': '#434141',
+      'link_text_lg': '#05B4A2',
+      'footer_color': '76A0BC',
+      'footer_para_text': 'F0F5F8',
+    },
     extend: {},
   },
   plugins: [],
