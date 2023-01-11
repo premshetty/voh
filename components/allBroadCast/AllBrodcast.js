914d218daf49bdeba78383e6754d3eaa79b2e41a
rising stars <risingstars@begig.io>
2023-01-11 17:39:14 +0530

    allbroadcast

---
+++ components/allBroadCast/AllBrodcast.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllBrodcasr = () => {
+  return (
+    <div>AllBrodcasr</div>
+  )
+}
+
+export default AllBrodcasr
---
+++ pages/allbroadcast/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AllBrodcast from '../../components/allBroadCast/AllBrodcast'
+
+const index = () => {
+  return (
+    <div>
+      <AllBrodcast/>
+    </div>
+  )
+}
+
+export default index
