c670ee3fd4c72f298344adb9d337a855a704b4ca
PREM KUMAR <premenaga@gmail.com>
2023-01-11 12:42:33 +0530

    newas and pr made responsive

--- components/newsandpr/NewsandPr.js
+++ components/newsandpr/NewsandPr.js
@@ -3,11 +3,11 @@ import { Nav } from '../editorials/Editorials'
 import classes from '../editorials/editorial.module.css'
 export const Card1 = () => {
     return (
-        <div className={`${classes.IdeaLeaderCard2} flex gap-5 relative h-[330px] mt-10 w-full pr-10 p-5`}>
-            <img className='h-full w-[40%] object-cover' src="/persons/news.png" alt="" />
+        <div className={`${classes.IdeaLeaderCard2} flex flex-col md:flex-row gap-5 relative md:h-[330px] mt-10 w-full pr-10 p-5`}>
+            <img className='h-full md:w-[40%] object-cover' src="/persons/news.png" alt="" />
             <div className=' flex flex-col gap-4 '>
-                <h2 className='font-bold text-[32px]'>No dearth of platelets, beds for dengue patients in UP: Dy CM Pathak ..</h2>
-                <p className='text-lg'>Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and beds for the Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and beds for the Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and beds for the ….. </p>
+                <h2 className='font-bold text-[24px] md:text-[32px]'>No dearth of platelets, beds for dengue patients in UP: Dy CM Pathak ..</h2>
+                <p className='text-sm md:text-lg'>Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and beds for the Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and beds for the Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and beds for the ….. </p>
                 <p className='text-[#606060] text-sm'>12 Oct, 2022</p>
             </div>
             <button className='h-10 w-32 bg-button_cta absolute rounded-tl-2xl bottom-0 right-0 font-bold text-white'>Read more</button>
@@ -16,13 +16,13 @@ export const Card1 = () => {
 }
 export const Card2 = () => {
     return (
-        <div className={`${classes.IdeaLeaderCard2} flex gap-5 relative h-[330px] w-[700px] pr-10 p-5`}>
+        <div className={`${classes.IdeaLeaderCard2} flex gap-5 relative h-[330px]  md:w-[700px] pr-10 p-5`}>
             <div className=' flex flex-col gap-4 '>
-                <h2 className='font-bold text-[28px]'>No dearth of platelets, beds for dengue patients in UP: Dy CM Pathak ..</h2>
+                <h2 className='font-bold text-[20px] md:text-[28px]'>No dearth of platelets, beds for dengue patients in UP: Dy CM Pathak ..</h2>
                 <p className='text-base'>Uttar Pradesh Deputy Chief Minister Brajesh Pathak on Thursday claimed there is no dearth of platelets and  </p>
                 <p className='text-[#606060] text-sm'>12 Oct, 2022</p>
             </div>
-            <img className='h-full w-[250px] rounded-2xl object-cover' src="/persons/news.png" alt="" />
+            <img className='h-full w-[150px] md:w-[250px] rounded-2xl object-cover' src="/persons/news.png" alt="" />
             <button className='h-10 w-32 bg-button_cta absolute rounded-tr-2xl bottom-0 left-0 font-bold text-white'>Read more</button>
         </div>
     )
@@ -31,14 +31,14 @@ const NewsandPr = () => {
     return (
         <>
             <Nav />
-            <div className='px-10 w-full mt-32'>
+            <div className='px-3 md:px-10 w-full mt-32'>
                 <div className='flex justify-between items-center border-b-[5px] pb-2 border-[#36AC9B]'>
                     <h2 className='font-bold text-xl'>News & PR</h2>
                 </div>
                 <Card1 />
                 <div className='mt-10'>
                     <h2 className='font-bold text-xl'>Latest New & PR</h2>
-                    <div className='grid grid-cols-2 gap-y-8 justify-items-stretch mt-10'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-y-8 justify-items-stretch mt-10'>
                         <Card2 />
                         <Card2 />
                         <Card2 />
@@ -49,11 +49,11 @@ const NewsandPr = () => {
                 </div>
                 <div className='mt-10'>
                     <h2 className='font-bold text-xl'>Treading</h2>
-                    <div className='flex mt-10 justify-between items-baseline'>
-                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} h-[180px] w-auto `} alt="" />
-                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} h-[180px] w-auto `} alt="" />
-                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} h-[180px] w-auto `} alt="" />
-                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} h-[180px] w-auto `} alt="" />
+                    <div className='flex mt-10 flex-wrap gap-5 justify-between items-baseline'>
+                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} md:h-[180px] w-auto `} alt="" />
+                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} md:h-[180px] w-auto `} alt="" />
+                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} md:h-[180px] w-auto `} alt="" />
+                        <img src="/persons/news.png" className={`${classes.IdeaLeaderCard2} md:h-[180px] w-auto `} alt="" />
                     </div>
                 </div>
             </div>
