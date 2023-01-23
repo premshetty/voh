3f1b21ef667300676019a479860cef57f7c794fd
PREM KUMAR <premenaga@gmail.com>
2023-01-24 03:19:37 +0530

    page build error fix

--- pages/articleandblogs/index.js
+++ pages/articleandblogs/index.js
@@ -6,3 +6,8 @@ const index = () => {
 }
 
 export default index
+export async function getServerSideProps(context) {
+    return {
+        props: {}, // will be passed to the page component as props
+    }
+}
